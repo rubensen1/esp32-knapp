3ae91d327b286a356f0cc260d3886d6306413689
ruby <ruben.vooren@gmail.com>
2025-10-07 13:34:20 +0200

    testing program

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,33 @@
 #include <Arduino.h>
 
+const int ledPin = 2;
+
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Start serial communication
+  Serial.begin(115200);
+  while (!Serial) { ; } // Vent til Serial er klar (for noen boards)
+  
+  // Sett LED-pinnen som output
+  pinMode(ledPin, OUTPUT);
+  
+  Serial.println("ESP32 starter...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Blink LED
+  digitalWrite(ledPin, HIGH);
+  Serial.println("LED ON");
+  delay(1000); // vent 1 sekund
+  
+  digitalWrite(ledPin, LOW);
+  Serial.println("LED OFF");
+  delay(1000); // vent 1 sekund
 }
 
 // put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
 }
-
-//hbbhuyuyuyi
-//dnwofnoew
-//hdwo
