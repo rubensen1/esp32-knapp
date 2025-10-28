b5594af02742609c901f70115f27285ebb0ef649
ruby <ruben.vooren@gmail.com>
2025-10-28 10:34:19 +0100

    merga yuhh

--- src/main.cpp
+++ src/main.cpp
@@ -16,26 +16,26 @@ WebServer server(80);
 
 DFRobot_RGBLCD1602 lcd(0x6B, 16, 2); 
 
-const int LED_PIN = 2;
+const int kulereLedPin = 2;
 bool ledState = false;
 
 
-  CRGB leds[NUM_LEDS];
-  
-  const int green_button_pin = 34;
-  const int yellow_button_pin = 36;
-  const int red_button_pin = 39;
+CRGB leds[NUM_LEDS];
 
-  const int sda_pin = 21;
-  const int scl_pin = 22;
+const int green_button_pin = 34;
+const int yellow_button_pin = 36;
+const int red_button_pin = 39;
 
-  const int green_led_pin = 17;
-  const int yellow_led_pin = 18;
-  const int red_led_pin = 19;
+const int sda_pin = 21;
+const int scl_pin = 22;
 
-  int green_button_state = 1;
-  int yellow_button_state = 1;
-  int red_button_state = 1;
+const int green_led_pin = 17;
+const int yellow_led_pin = 18;
+const int red_led_pin = 19;
+
+int green_button_state = 1;
+int yellow_button_state = 1;
+int red_button_state = 1;
 
 void handleNotFound() {
   server.send(404, "text/plain", "Not found");
@@ -75,26 +75,22 @@ void handleScreen() {
 void setup() {
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
   
-pinMode(green_button_pin, INPUT);
-pinMode(yellow_button_pin, INPUT);
-pinMode(red_button_pin, INPUT);
+  pinMode(green_button_pin, INPUT);
+  pinMode(yellow_button_pin, INPUT);
+  pinMode(red_button_pin, INPUT);
 
-pinMode(green_led_pin, OUTPUT);
-pinMode(yellow_led_pin, OUTPUT);
-pinMode(red_led_pin, OUTPUT);
+  pinMode(green_led_pin, OUTPUT);
+  pinMode(yellow_led_pin, OUTPUT);
+  pinMode(red_led_pin, OUTPUT);
 
-pinMode(sda_pin, OUTPUT);
-pinMode(scl_pin, OUTPUT);
-
-Serial.begin(115200);
-
-
-
-
+  pinMode(sda_pin, OUTPUT);
+  pinMode(scl_pin, OUTPUT);
 
   Serial.begin(115200);
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, HIGH);
+
+  Serial.begin(115200);
+  pinMode(kulereLedPin, OUTPUT);
+  digitalWrite(kulereLedPin, HIGH);
   lcd.setRGB(0, 0, 255);
 
   lcd.init();            // Initialiser skjermen
