afa6b7e453811637fd8b265d666fc1ce1c0d0092
ruby <ruben.vooren@gmail.com>
2025-10-28 14:52:41 +0100

    YUHHHHHHHHHHHH FUNGERER RAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHpenis

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,37 @@
+#include <Arduino.h>
 #include <WiFi.h>
 #include <WebServer.h>
 #include <SPIFFS.h>
 #include <ESPmDNS.h>
 #include <DFRobot_RGBLCD1602.h>
 #include <ArduinoJson.h>
-// #include <FastLED.h>
 
+// LED strip definisjon (fra første program)
 #define LED_PIN 4
 #define NUM_LEDS 20
 
-const char* ssid = "phone1";
-const char* password = "phone123";
-
-WebServer server(80);
-
-DFRobot_RGBLCD1602 lcd(0x6B, 16, 2); 
-
-// CRGB leds[NUM_LEDS];
-
+// Knapp pins
 const int green_button_pin = 34;
 const int yellow_button_pin = 36;
 const int red_button_pin = 39;
 
+// LED pins
 const int green_led_pin = 17;
 const int yellow_led_pin = 18;
 const int red_led_pin = 19;
 
+// Knapp states
 int green_button_state = 1;
 int yellow_button_state = 1;
 int red_button_state = 1;
+
+// WiFi credentials
+const char* ssid = "phone1";
+const char* password = "phone123";
+
+// WebServer og LCD
+WebServer server(80);
+DFRobot_RGBLCD1602 lcd(0x6B, 16, 2);
 
 void handleNotFound() {
   server.send(404, "text/plain", "Not found");
@@ -47,7 +50,6 @@ void handleScreen() {
       // Viser første spørsmål på LCD
       if (questions.size() > 0) {
         String question = questions[0];
-        // Hvis spørsmålet er lengre enn 16 tegn, vis bare første 16
         lcd.setCursor(0, 0);
         lcd.print(question.substring(0, 16));
         if (question.length() > 16) {
@@ -66,10 +68,7 @@ void handleScreen() {
 }
 
 void setup() {
-  // FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
-  Serial.begin(115200);
-  Serial.println("ESP32 start!");
-  
+  // Knapper og LED setup
   pinMode(green_button_pin, INPUT);
   pinMode(yellow_button_pin, INPUT);
   pinMode(red_button_pin, INPUT);
@@ -78,13 +77,16 @@ void setup() {
   pinMode(yellow_led_pin, OUTPUT);
   pinMode(red_led_pin, OUTPUT);
 
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, HIGH);
+  // Serial
+  Serial.begin(115200);
+  delay(1000);
 
+  Serial.println("ESP32 starter");
+  
+  // I2C og LCD
   Wire.begin();
-  lcd.init();            // Initialiser skjermen
+  lcd.init();
   lcd.setRGB(0, 0, 255);
-
   lcd.setCursor(0, 0);
   lcd.print("Hello world!");
   lcd.setCursor(0, 1);
@@ -97,18 +99,32 @@ void setup() {
   }
 
   // WiFi
+  Serial.println("Starting WiFi connection...");
   WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi");
-  while (WiFi.status() != WL_CONNECTED) {
+  Serial.print("Connecting to WiFi: ");
+  Serial.println(ssid);
+  
+  int timeout = 0;
+  while (WiFi.status() != WL_CONNECTED && timeout < 20) {
     delay(300);
     Serial.print(".");
+    timeout++;
   }
+
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("\nWiFi connection failed!");
+    Serial.println("Check SSID and password");
+    return;
+  }
+
   Serial.println("\nConnected! IP: " + WiFi.localIP().toString());
 
+  // mDNS
   if (MDNS.begin("esp32")) {
     Serial.println("mDNS responder started: http://esp32.local/");
   }
 
+  // WebServer routes
   server.on("/api/screen", HTTP_POST, handleScreen);
   server.serveStatic("/", SPIFFS, "/index.html");
   server.serveStatic("/styles.css", SPIFFS, "/styles.css");
@@ -116,53 +132,40 @@ void setup() {
   server.onNotFound(handleNotFound);
 
   server.begin();
+  Serial.println("WebServer startet");
 }
 
 void loop() {
+  // Håndter webserver
   server.handleClient();
 
-
+  // Les knappestatus
   int green_button_state = digitalRead(green_button_pin);
   int yellow_button_state = digitalRead(yellow_button_pin);
   int red_button_state = digitalRead(red_button_pin);
-  Serial.println((green_button_state, yellow_button_state, red_button_state));
   
+  Serial.println("ballalalalal");
 
+  // Styr LED-er basert på knapper
   if (green_button_state == 1) {
     Serial.println("Green button");
     digitalWrite(green_led_pin, LOW);
     digitalWrite(yellow_led_pin, HIGH);
     digitalWrite(red_led_pin, HIGH);
 
-    // for (int i = 0; i <= 19; i++) {
-    // leds[i] = CRGB (0, 255,0);
-    // }
-    // FastLED.show();
-
   } else if (yellow_button_state == 1) {
-     Serial.println("Yellow button");
-     digitalWrite(green_led_pin, HIGH);
-     digitalWrite(yellow_led_pin, LOW);
-     digitalWrite(red_led_pin, HIGH);
-
-    // for (int i = 0; i <= 19; i++) {
-    // leds[i] = CRGB (255, 255,0);
-    // }
-    // FastLED.show();
+    Serial.println("Yellow button");
+    digitalWrite(green_led_pin, HIGH);
+    digitalWrite(yellow_led_pin, LOW);
+    digitalWrite(red_led_pin, HIGH);
 
   } else if (red_button_state == 1) {
     Serial.println("Red button");
     digitalWrite(green_led_pin, HIGH);
     digitalWrite(yellow_led_pin, HIGH);
     digitalWrite(red_led_pin, LOW);
-
-    // for (int i = 0; i <= 19; i++) {
-    //   leds[i] = CRGB (255, 0,0);
-    // }
-    // FastLED.show();
-  }
-
-  else {
+    
+  } else {
     digitalWrite(green_led_pin, HIGH);
     digitalWrite(yellow_led_pin, HIGH);
     digitalWrite(red_led_pin, HIGH);
